1793cec13ae6646f638170ff91820a20a4d0846c
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2021-11-24 18:33:17 -0500

    Fix py36 incompatibility with pathlib usage

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -9,7 +9,7 @@ def fakedb(tmp_path):
     """Create a temporary pho-database (fakedb) for testing fields"""
 
     sqlite = peewee.SqliteDatabase(
-        tmp_path / f"{uuid.uuid4()}.db",
+        str(tmp_path / f"{uuid.uuid4()}.db"),
         pragmas={
             "journal_mode": "wal",
             "cache_size": -1 * 64000,
