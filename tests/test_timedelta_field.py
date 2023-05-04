b0ac2f92d7e184b3abf6131fe6340dab3a26e27a
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2023-05-04 14:35:01 -0400

    Add tests for timedelta field

---
+++ tests/test_timedelta_field.py
@@ -0,0 +1,31 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=missing-class-docstring
+# pylint: disable=too-few-public-methods
+# pylint: disable=unused-import
+import datetime
+from pathlib import Path
+
+import peewee
+
+import peewee_plus
+from .fixtures import fakedb
+
+
+def test_conversion(fakedb):
+    """Test basic usage of PathField for roundtrip compatibility"""
+
+    class TestModel(peewee.Model):
+        class Meta:
+            database = fakedb
+
+        name = peewee.CharField()
+        some_timedelta = peewee_plus.TimedeltaField()
+
+    fakedb.create_tables([TestModel])
+
+    delta = datetime.timedelta(seconds=300)
+    model = TestModel(name="one", some_timedelta=delta)
+    model.save()
+
+    new = TestModel.get(TestModel.name == "one")
+    assert new.some_timedelta == delta
